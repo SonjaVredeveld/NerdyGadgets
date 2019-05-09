b4556c6404f2c084af53b4d32f10b884cf0dd3b0
Niek Nijland <43474728+NiekNijland@users.noreply.github.com>
2019-05-09 09:57:29 +0200

    Skelet gemaakt voor de 4 schermen
    Skelet opgezet volgens het klassendiagram na controle van gerben

---
+++ PlannerScreen.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kbs2;
+
+import javax.swing.JTable;
+
+/**
+ *
+ * @author Niek J Nijland
+ */
+public class PlannerScreen {
+    private JTable JTOrderList = new JTable();
+}
