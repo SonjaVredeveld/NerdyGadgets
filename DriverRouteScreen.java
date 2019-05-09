5c876ab1eb1de89b19d320bd3d34f61e4f837ff6
Niek Nijland <43474728+NiekNijland@users.noreply.github.com>
2019-05-09 10:13:33 +0200

    Skelet schermen gemaakt deel 2
    de rest van de schermen ook gemaakt volgens het klassendiagram

---
+++ DriverRouteScreen.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kbs2;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTable;
+
+/**
+ *
+ * @author Niek J Nijland
+ */
+public class DriverRouteScreen extends JFrame implements ActionListener{
+    private JTable AvailableRouteLocations = new JTable();
+    private Route route = new Route();
+    private JButton JBCancel = new JButton();
+    private JButton JBSubmit = new JButton();
+    
+    public DriverRouteScreen() {
+        
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+        
+}
