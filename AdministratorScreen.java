14ba6bbc2d5ea78b080f4591854205d452c0d64b
SonjaVredeveld <sonjavredeveld@gmail.com>
2019-05-10 12:59:58 +0200

    Start AdminScreen

--- AdministratorScreen.java
+++ AdministratorScreen.java
@@ -1,33 +1,110 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package kbs2;
 
+package nerdyg;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JFrame;
-import javax.swing.JTabbedPane;
-import javax.swing.JTable;
+import javax.swing.*;
 
-/**
- *
- * @author Niek J Nijland
- */
 public class AdministratorScreen extends JFrame implements ActionListener{
-    private JTabbedPane JTPAdminTabs = new JTabbedPane();
-    private JTable JTStock = new JTable();
-    private JTable JTCustomers = new JTable();
-    private JTable JTOrders = new JTable();
+    private JTabbedPane JTPAdminTabs;
+    private JTable JTStock;
+    private JTable JTCustomers;
+    private JTable JTOrders;
+    private JFrame frame;
+    private JButton JBlogout;
     
     public AdministratorScreen() {
+    frame = new JFrame();    
+    frame.setTitle("Systeembeheer");
+    frame.setLayout(new FlowLayout());
+    
+    // data in PRODUCTEN tab 
+    
+    String dataProducten[][] = { 
+        {"123","dit","Prijs", "edit"},    
+        {"1234","Shirt","Prijs", "edit"}, 
         
-    }
+    }; 
+    
+    String columnProducten[] = {"Product_ID","Productnaam","Prijs p/s", "edit"};         
+    
+    JTStock = new JTable(dataProducten,columnProducten);
+    JTStock.setFillsViewportHeight(true);
+    
+    JScrollPane spProducten = new JScrollPane(JTStock); 
+    spProducten.setPreferredSize(new Dimension(775,450));
+    JPanel panelProducten = new JPanel();
+        panelProducten.setLayout(new BorderLayout());
+        panelProducten.add(spProducten);
+        
+        
+    // data in KLANTGEGEVENS tab
+    
+    String dataKlanten[][] = { 
+        {"Voornaam", "Achternaam", "Adres", "Woonplaats", "Afleveradres", "edit"},    
+        {"Voornaam", "Achternaam", "Adres", "Woonplaats", "Afleveradres", "edit"}, 
+        
+    }; 
+    
+    String columnKlanten[] = {"Voornaam", "Achternaam", "Adres", "Woonplaats", "Afleveradres", "edit"};
+    
+    JTCustomers = new JTable(dataKlanten,columnKlanten);   
+    JTCustomers.setFillsViewportHeight(true);
+    
+    JScrollPane spCustomers = new JScrollPane(JTCustomers); 
+    JPanel panelCustomers = new JPanel();
+        panelCustomers.setLayout(new BorderLayout());
+        panelCustomers.add(spCustomers);
+        
+        
+    // data in ORDERS tab 
+    
+    String dataOrders[][] = { 
+        {"1234", "765", "443", "Klantnaam"}, 
+        {"1234", "765", "443", "Klantnaam"},
+        {"1234", "765", "443", "Klantnaam"},
+        {"1234", "765", "443", "Klantnaam"},
+    }; 
+    
+    String columnOrders[] = {"Ordernummer", "Productnummer", "KlantID", "Klantnaam"};
+    
+    JTOrders = new JTable(dataOrders,columnOrders);   
+    JTOrders.setFillsViewportHeight(true);
+    
+    JScrollPane spOrders = new JScrollPane(JTOrders); 
+    JPanel panelOrders = new JPanel();
+        panelOrders.setLayout(new BorderLayout());
+        panelOrders.add(spOrders);
+    
+    JTPAdminTabs = new JTabbedPane();
+    JTPAdminTabs.add("Producten", panelCustomers);
+    JTPAdminTabs.add("Klantgegevens", panelProducten);
+    JTPAdminTabs.add("Ordegegevens", panelOrders);
+    
+
+    
+    
+    // LOGOUT button
+    JBlogout = new JButton("Logout");
+    
+    
+    
+    frame.add(JTPAdminTabs);
+    frame.add(JBlogout);
+    frame.setSize(800,600);    
+    frame.setVisible(true);
+}
+    
+    
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+        if (e.getSource() == JBlogout) {
+            dispose();
+        }
     }
     
     
