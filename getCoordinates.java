f2a16a5333ed14c7d86f4d64c7f8eaf0ca90036c
Pieter van de Vis <pietervandevis@hotmail.com>
2019-05-17 10:26:05 +0200

    dingen

--- getCoordinates.java
+++ getCoordinates.java
@@ -6,9 +6,12 @@ import java.util.Map;
 
 public class getCoordinates {
 
+    // http://julien.gunnm.org/geek/programming/2015/09/13/how-to-get-geocoding-information-in-java-without-google-maps-api/
+    // https://dzone.com/articles/google-maps-java-swing
 
     private String[] address;
     private String ad;
+    private String CID;
 
     public getCoordinates(){
         this.address = getDbAddress();
@@ -21,15 +24,11 @@ public class getCoordinates {
     public String[] getDbAddress(){
 
 //        System.out.println("1");
-        ArrayList<ArrayList<String>> rows = DBConnection.selectQuery("SELECT DeliveryAddressLine2 FROM customers WHERE CustomerID > 1055");
+        ArrayList<ArrayList<String>> rows = DBConnection.selectQuery("SELECT DeliveryAddressLine2, CustomerID FROM customers WHERE CustomerID > 1055");
 //        System.out.println("1");
         String[] result = new String[rows.size()];
 
         for(int i = 0; i < rows.size(); i++){
-            ArrayList<String> row = rows.get(i);
-//            System.out.println(row);
-            this.ad = row.get(0);
-//            System.out.println(ad);
 
             result[i] = lonlat(rows.get(i));
 
@@ -38,18 +37,20 @@ public class getCoordinates {
         return result;
     }
 
-    public String lonlat(ArrayList<String> buh){
+
+    public String lonlat(ArrayList<String> AddressList){
         Map<String, Double> coords;
-//        System.out.println("2");
-        String ListString = "";
+        String CustomerID = AddressList.get(1);
+        coords = OpenStreetMapUtils.getInstance().getCoordinates(AddressList.get(0));
 
-        for (String s: buh){
-            ListString += s;
+        int update = DBConnection.executeQuery("UPDATE customers SET latitude = "+coords.get("lat")+", longitude = "+coords.get("lon")+" WHERE CustomerID =  "+CustomerID+"");
+        if (update > 0) {
+            System.out.println("we updated the item.");
+        } else {
+            System.out.println(DBConnection.statusMsg); //check status(also error info)
         }
-//        System.out.println(ListString);
-        coords = OpenStreetMapUtils.getInstance().getCoordinates(ListString);
 
-        return("latitude :" + coords.get("lat"))+(" longitude:" + coords.get("lon"));
+        return("latitude :" + coords.get("lat"))+(" longitude:" + coords.get("lon")+ ", " + CustomerID);
     }
 
     public String[] getAddress() {
@@ -58,6 +59,10 @@ public class getCoordinates {
 
     public static void main(String[] args) {
         String[] instance = new getCoordinates().getAddress();
+
+//        UPDATE table_name
+//        SET column1 = value1, column2 = value2, ...
+//        WHERE condition;
 
     }
 }
