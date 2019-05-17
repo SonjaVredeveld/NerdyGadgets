734adae1b01996be7f3fbf7e25f14d8b79d32cd5
Niek Nijland <43474728+NiekNijland@users.noreply.github.com>
2019-05-17 09:53:09 +0200

    doorloop verbeterd

--- PlannerScreen.java
+++ PlannerScreen.java
@@ -121,7 +121,7 @@ public class PlannerScreen extends JFrame implements ActionListener, TableModelL
         }else if(ar1.size() <= 20){
             Route r1 = new Route(ar1); 
             if(r1.getResult()){
-                new RouteCreatedPopup(this);
+                new RouteScreen(this,r1);
             }else{
                 JOptionPane.showMessageDialog(this,"Er ging iets fout bij het bereken van uw route");
             }
