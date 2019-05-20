781e26951f00858f25823800f182923a164f26b5
Pieter van de Vis <pietervandevis@hotmail.com>
2019-05-20 15:29:09 +0200

    oops foutje met merge, is nu opgelost
    was een haakje vergeten

--- DriverScreen.java
+++ DriverScreen.java
@@ -36,8 +36,6 @@ public class DriverScreen extends JFrame implements ActionListener, TableModelLi
         jtTitle.setPreferredSize(new Dimension(800, 25));
         jtTitle.setHorizontalAlignment(JLabel.CENTER);
 
-        this.user = user;
-
         //Test Column names
         String[] columnNames = {"Route nummer", "Aantal locaties", "Afstand", "Bekijk route", "Datum aangemaakt"};
 
@@ -49,45 +47,46 @@ public class DriverScreen extends JFrame implements ActionListener, TableModelLi
             Route route = routes.get(i);
             Object[] row = {route.getID(), route.getLocations().size(), route.getDistance(), "Bekijk route", route.getCreationDate()};
             columnData[i] = row;
-
-            //Table Layout
-            jtRouteTable = new JTable(columnData, columnNames);
-            TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(jtRouteTable.getModel());
-            jtRouteTable.setRowSorter(sorter);
-
-            jtRouteTable.getModel().addTableModelListener(this);
-
-            tableSP = new JScrollPane(jtRouteTable);
-            tableSP.setPreferredSize(new Dimension(770, 450));
-            tableSP.setAlignmentX(LEFT_ALIGNMENT);
-
-            jtRouteTable.getColumn("Bekijk route").setCellRenderer(new ButtonRenderer());
-            jtRouteTable.getColumn("Bekijk route").setCellEditor(new ButtonEditor(new JCheckBox()));
-            // Turns columns ''Bekijk route" into buttons (more info found in ButtonRenderer and ButtonEditor)
-
-            jbLogout = style.button("Log uit");
-            jbLogout.addActionListener(this);
-
-            Panel p = new Panel();
-            p.setLayout(new GridLayout(1, 5));
-            p.setPreferredSize(new Dimension(800, 50));
-
-            // Order for elements to appear
-            add(jtTitle);
-            add(tableSP);
-            p.add(new JLabel(" "));
-            p.add(new JLabel(" "));
-            p.add(new JLabel(" "));
-            p.add(new JLabel(" "));
-            p.add(jbLogout);
-            add(p);
-
-            setResizable(false);
-            setVisible(true);
-            pack();
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         }
+
+        //Table Layout
+        jtRouteTable = new JTable(columnData, columnNames);
+        TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(jtRouteTable.getModel());
+        jtRouteTable.setRowSorter(sorter);
+
+        jtRouteTable.getModel().addTableModelListener(this);
+
+        tableSP = new JScrollPane(jtRouteTable);
+        tableSP.setPreferredSize(new Dimension(770, 450));
+        tableSP.setAlignmentX(LEFT_ALIGNMENT);
+
+        jtRouteTable.getColumn("Bekijk route").setCellRenderer(new ButtonRenderer());
+        jtRouteTable.getColumn("Bekijk route").setCellEditor(new ButtonEditor(new JCheckBox()));
+        // Turns columns ''Bekijk route" into buttons (more info found in ButtonRenderer and ButtonEditor)
+
+        jbLogout = style.button("Log uit");
+        jbLogout.addActionListener(this);
+
+        Panel p = new Panel();
+        p.setLayout(new GridLayout(1, 5));
+        p.setPreferredSize(new Dimension(800, 50));
+
+        // Order for elements to appear
+        add(jtTitle);
+        add(tableSP);
+        p.add(new JLabel(" "));
+        p.add(new JLabel(" "));
+        p.add(new JLabel(" "));
+        p.add(new JLabel(" "));
+        p.add(jbLogout);
+        add(p);
+
+        setResizable(false);
+        setVisible(true);
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+
     private void logout() {
         ActiveUser = null;
         this.dispose();
