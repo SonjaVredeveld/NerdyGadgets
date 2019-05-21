85b6eae9b00dc66ba520ba683da25fbff60c3fd5
Pieter92 <44613438+Pieter92@users.noreply.github.com>
2019-05-21 11:36:00 +0200

    Update AdministratorScreen.java
    Co-Authored-By: lorenzovdo <lorenzovdoudenrijn73@gmail.com>

--- AdministratorScreen.java
+++ AdministratorScreen.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import javax.swing.event.*;
 import javax.swing.table.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.*;
 import java.util.ArrayList;
 
